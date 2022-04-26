12bf964c11a44bee6d4edd352ddcba163a699889
zhuxiangfa <zhuxiangfa@huya.com>
2022-04-26 10:29:26 +0800

    chore:add observer-event-sdk-version example

---
+++ examples/observer-event-sdk-version/streamer/app.js
@@ -0,0 +1,17 @@
+import { UI } from "@hyext/hy-ui";
+import React, { Component } from "react";
+import { Example } from "./common";
+import "./app.hycss";
+
+const { View, Text } = UI;
+class App extends Component {
+  render() {
+    return (
+      <View className="container">
+        <Example />
+      </View>
+    );
+  }
+}
+
+export default App;
