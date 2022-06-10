e80fb4ad86b2d8f769365b10f56b69fe2122451e
maizhiying <zhiying.mai@gmail.com>
2022-06-10 10:01:51 +0800

    chore: update streamer-control

--- examples/streamer-control/src/common.js
+++ examples/streamer-control/src/common.js
@@ -8,6 +8,8 @@ const { View, Text, Button } = UI
 export const Example = () => {
   const [showResult, setShowResult] = useState('未开始')
   const [hideResult, setHideResult] = useState('未开始')
+  const [loadResult, setLoadesult] = useState('未开始')
+  const [unloadResult, setUnloadResult] = useState('未开始')
 
   const showEntrance = async () => {
     try {
@@ -31,12 +33,46 @@ export const Example = () => {
       setHideResult(`失败：${err.message}`)
     }
   }
+  const loadPopup = async () => {
+    try {
+      setLoadesult('处理中')
+      await hyExt.backend.commonOperate({
+        key: 'controlPanelLoad',
+        param: {
+          extTypes: 'web_popup',
+          visible: true,
+        },
+      })
+      setLoadesult('成功')
+    } catch (err) {
+      setLoadesult(`失败：${err.message}`)
+    }
+  }
+  const unloadPopup = async () => {
+    try {
+      setUnloadResult('处理中')
+      await hyExt.backend.commonOperate({
+        key: 'controlPanelLoad',
+        param: {
+          extTypes: 'web_popup',
+          visible: false,
+        },
+      })
+      setUnloadResult('成功')
+    } catch (err) {
+      setUnloadResult(`失败：${err.message}`)
+    }
+  }
   return (
     <View className='example'>
       <Button onPress={showEntrance}><Text>显示入口</Text></Button>
       <Text>显示入口结果：{showResult}</Text>
       <Button onPress={hideEntrance}><Text>隐藏入口</Text></Button>
       <Text>隐藏入口结果：{hideResult}</Text>
+      <Button onPress={loadPopup}><Text>加载浮窗</Text></Button>
+      <Text>加载浮窗结果：{loadResult}</Text>
+      <Button onPress={unloadPopup}><Text>卸载浮窗</Text></Button>
+      <Text>卸载浮窗结果：{unloadResult}</Text>
     </View>
   )
 }
