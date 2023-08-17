03f19a5242e3e956915349b867050a3b25845f52
maizhiying <zhiying.mai@gmail.com>
2023-08-17 16:48:16 +0800

    chore: exe

---
+++ examples/exe/src/config.js
@@ -0,0 +1,11 @@
+var config = {
+  // 必须要填写，并且是业务唯一的,业务后台会根据这个来获取game的下载配置信息
+  "game_name": "游戏Demo",
+  // zip包的地址
+  "game_url": "https://g1c972e2182629c7-59ku7pqm.hyext.com/extResource/GameDemo.zip",
+  // zip包的md5
+  "game_md5": "f5fc57a45bdb293d2e298de50022bc2e",
+  // zip包解压之后的文件名
+  "game_exe_name": "GameDemo.exe"
+}
+export default config
